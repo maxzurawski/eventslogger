0e024b4ccb5f66b49e40b478e174c0d4c006f80d
Max Zurawski <maksymilian.zurawski@consileon.pl>
2019-10-13 23:48:41 +0200

    Applying FindEvents in main func

---
+++ eventslogger.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/labstack/echo"
+	"github.com/xdevices/eventslogger/config"
+	"github.com/xdevices/eventslogger/dbprovider"
+	"github.com/xdevices/eventslogger/handlers"
+	"github.com/xdevices/eventslogger/service"
+)
+
+func main() {
+	e := echo.New()
+	e.GET("/events/", handlers.FindEventsHandler)
+	e.Logger.Fatal(e.Start(config.EventsloggerConfigManager().Address()))
+}
+
+func init() {
+	manager := config.InitEventsloggerEurekaManager()
+	manager.SendRegistrationOrFail()
+	manager.ScheduleHeartBeat(config.EventsloggerConfigManager().ServiceName(), 10)
+	dbprovider.InitDbManager()
+	service.Init()
+}
