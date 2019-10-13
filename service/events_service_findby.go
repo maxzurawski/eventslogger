0971cab212930c7315de721a5c3957e8b6d45750
Max Zurawski <maksymilian.zurawski@consileon.pl>
2019-10-13 23:39:34 +0200

    FindBy func

---
+++ service/events_service_findby.go
@@ -0,0 +1,14 @@
+package service
+
+import "github.com/xdevices/eventslogger/dto"
+
+func (s *service) FindBy(dto dto.SearchDTO) (result []dto.EventDTO, err error) {
+	events, err := s.mgr.FindBy(dto)
+	if err != nil {
+		return
+	}
+	for _, item := range events {
+		result = append(result, s.mgr.MapToDto(&item))
+	}
+	return
+}
